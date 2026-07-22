9a0ef604e33f2dda9ceb37761f8432085ad4462d
alessandraramirezgil-cloud <alessandraramirezgil@gmail.com>
2026-07-22 16:00:19 -0500

    Update calculadora.py
    AGREGAR LA RESTAQ 
    Alessandra Gil

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,18 @@
-## CALCULADORA - VERSIÓN 1
-# Autor(a): NOMBRE APELLIDO
+## CALCULADORA - VERSIÓN 2
+# Autor(a): Alessandra Gil
 # ===========================================
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
 
-suma = num1 + num2
+print("\n1.Sumar)
+print("\n2.Restar)
 
-print("Resultado:", suma)
+opcion=input ("\n selecione una opcion")
+
+if opcion == "1":
+    print("\n La suma es:",num1 + num2)
+
+elif opcion =="2"
+    print("\n La resta es:", num1-num2)
+else:
+print("\n opcion no valida".)
